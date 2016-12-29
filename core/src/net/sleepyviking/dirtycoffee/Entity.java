258be3a31c610593812c8a4a2ac860ade71f8394
Nikoraito <nikoraito@gmail.com>
2016-12-29 00:44:02 -0500

    Added Entity

---
+++ core/src/net/sleepyviking/dirtycoffee/Entity.java
@@ -0,0 +1,80 @@
+package net.sleepyviking.dirtycoffee;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Dauas on 6/26/2016.
+ */
+public class Entity {
+
+    private int maxhealth;
+    private int health;
+    private int weight;
+    private int resistance;
+    private Vector2 position;
+    private String name;
+    private String spritename;
+
+    public int getMaxHealth(){
+        return maxhealth;
+    }
+    public void setMaxHealth(int n){
+        maxhealth = n;
+    }
+    public void incMaxHealth(int n){
+        maxhealth += n;
+    }
+    public void decMaxHealth(int n){
+        maxhealth -= n;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+    public void setHealth(int n){
+        maxhealth = n;
+    }
+    public void incHealth(int n){
+        health += n;
+    }
+    public void decHealth(int n){
+        health -= n;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+    public void setWeight(int n){
+        weight = n;
+    }
+    public void incWeight(int n){
+        weight += n;
+    }
+    public void decWeight(int n){
+        weight -= n;
+    }
+
+    public int getResistance(){
+        return resistance;
+    }
+    public void setResistance(int n){
+        resistance = n;
+    }
+    public void incResistance(int n){
+        resistance += n;
+    }
+    public void decResistance(int n){
+        resistance -= n;
+    }
+
+    public Vector2 getPosition(){
+        return position;
+    }
+    public void setPosition(Vector2 n) {
+        position.set(n);
+    }
+    public void setPosition(float x, float y) {
+        position.set(x, y);
+    }
+
+}
