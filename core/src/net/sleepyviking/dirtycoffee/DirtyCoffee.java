02741efd3c0601e07483e4aefb58ed5ee9e83616
Dauas <mime490@gmail.com>
2016-12-31 01:11:11 -0500

    Alliance Token Sprite Sheet
    Alliance Token Sprite Sheet created and turned into indivisual graphics
    in this commit...

--- core/src/net/sleepyviking/dirtycoffee/DirtyCoffee.java
+++ core/src/net/sleepyviking/dirtycoffee/DirtyCoffee.java
@@ -3,10 +3,12 @@ package net.sleepyviking.dirtycoffee;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+//import com.badlogic.gdx.graphics;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 
 public class DirtyCoffee extends ApplicationAdapter {
@@ -17,22 +19,29 @@ long startTime;
 private SpriteBatch batch;
 private Texture img;
 private Sprite sprite;
-
-//int xCenter = Gdx.graphics.getWidth()/2; Causes Null Pointer Exception...
-//int yCenter = Gdx.graphics.getHeight()/2;
+private TextureRegion[] alliance;
+private int xCenter;
+private int yCenter;
 
 	@Override
 	public void create () {
 		log("Creating Application...");
 		batch = new SpriteBatch();
-		img = new Texture("SnakeAbomination.png");
+		img = new Texture("allianceTokenSpritesheet.png");
 		img.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+		TextureRegion[][] tempSorting = TextureRegion.split(img,16, 16);
+		alliance = new TextureRegion[4];
+		int index = 0;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				alliance[index++] = tempSorting[i][j];
+			}
+		}
 		sprite = new Sprite(img);
-		startTime = System.currentTimeMillis();
-		//System.out.println(Gdx.files.getLocalStoragePath());
-		log("startTime set to " + startTime);
 		winWidth = Gdx.graphics.getWidth();
 		winHeight = Gdx.graphics.getHeight();
+		xCenter = winWidth/2;
+		yCenter = winHeight/2;
 		log("Width = "+ winWidth);
 		log("Height = "+ winHeight);
 		log("Done.");
@@ -43,6 +52,7 @@ private Sprite sprite;
 	public void dispose() {
 		/**Handle's Closing procedure's, Called when Application is manually closed or at any Exit Point within the code**/
 		log("Game is Closing...");
+		batch.dispose();
 		System.exit(0);
 	}
 
@@ -83,9 +93,11 @@ private Sprite sprite;
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 			batch.begin();
 			/**Draw Everything in Here, Stop Drawing before batch.end()**/
-			//batch.draw(img, xCenter - img.getWidth()/2, yCenter - img.getHeight()/2); //Dependent on Error in line 19
-			//batch.draw(sprite, sprite.getWidth()/2,sprite.getHeight()/2);
-			batch.draw(sprite, 0, 0);
+			batch.draw(sprite, xCenter - img.getWidth()/2, yCenter - img.getHeight()/2); //Dependent on Error in line 19
+			batch.draw(alliance[0], 0,0);
+			batch.draw(alliance[1], 50,0);
+			batch.draw(alliance[2], 100,0);
+			batch.draw(alliance[3], 150,0);
 			/**Stop Drawing**/
 			batch.end();
 
